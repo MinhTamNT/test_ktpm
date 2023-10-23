5ea7a5431b0561e59ad1631fa220e4364f9ac22d
PhuongLinh82 <minhtam78945@gmail.com>
2023-10-23 16:52:09 +0700

    54 them 1 dong them branch feature3-new day len github

--- thongbao.cpp
+++ thongbao.cpp
@@ -2,8 +2,9 @@
 using namespace std;
 int main{
 	int a = 5;
-	int b = 5
-	cout << "Tong phep toan = " << a + b << endl;
+	int b = 5;
+	int c = 10;
+	cout << "Tong phep toan = " << a + b + c << endl;
 	
 	return 0;
 	
