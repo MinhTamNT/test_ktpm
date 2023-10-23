8c869149104a96ccabe912517224e7cf3219b85e
PhuongLinh82 <2151013049linh@ou.edu.vn>
2023-10-23 16:19:05 +0700

    54 da sua file thong bao cpp thanh chuong trinh pheo toan tren nhanh fearure2-new

--- thongbao.cpp
+++ thongbao.cpp
@@ -1,7 +1,10 @@
 #include<iostream>
 using namespace std;
 int main{
-	cout << "Hello World World" << endl;
+	int a = 5;
+	int b = 5
+	cout << "Tong phep toan = " << a + b << endl;
+	
 	return 0;
 	
 }
